6d63a951db1f23a5af521f449bf0e3f7a688bfca
freek <freek@spatie.be>
2017-05-23 09:39:03 +0200

    update tests

--- tests/EmojiTest.php
+++ tests/EmojiTest.php
@@ -3,26 +3,33 @@
 namespace Spatie\Emoji\Test;
 
 use Spatie\Emoji\Emoji;
+use PHPUnit\Framework\TestCase;
 use Spatie\Emoji\Exceptions\UnknownCharacter;
 
-class EmojiTest extends \PHPUnit_Framework_TestCase
+class EmojiTest extends TestCase
 {
     /** @test */
     public function it_can_return_an_emoji_character_when_given_a_constant()
     {
-        $this->assertSame("\u{1F600}", Emoji::CHARACTER_GRINNING_FACE);
+        $this->assertSame("😀", Emoji::CHARACTER_GRINNING_FACE);
     }
 
     /** @test */
-    public function it_provides_a_conveniece_method_to_return_emoji_characters()
+    public function it_provides_a_convenience_method_to_return_emoji_characters()
     {
-        $this->assertSame("\u{1F600}", Emoji::grinningFace());
+        $this->assertSame("😀", Emoji::grinningFace());
+    }
+
+    /** @test */
+    public function it_can_return_the_fitz_patrick_emoji()
+    {
+        $this->assertSame("🏻", Emoji::emojiModifierFitzpatrickType12());
     }
 
     /** @test */
     public function it_will_throw_an_exception_when_getting_unknown_characters()
     {
-        $this->setExpectedException(UnknownCharacter::class);
+        $this->expectException(UnknownCharacter::class);
 
         Emoji::thisCharacterDoesNotExist();
     }
