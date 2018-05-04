d5797158b1805bc7a81c504c306726b0a30073a3
Freek Van der Herten <freek@spatie.be>
2018-05-04 15:59:37 +0000

    Apply fixes from StyleCI

--- src/Exceptions/UnknownCharacter.php
+++ src/Exceptions/UnknownCharacter.php
@@ -6,7 +6,7 @@ use Exception;
 
 class UnknownCharacter extends Exception
 {
-    public static function create($character) : UnknownCharacter
+    public static function create($character) : self
     {
         return new static("Character `{$character}` does not exist");
     }
