38d7baf897b0b21fcd228dbee74c960b4a5d9d6a
Gummibeer <dev.gummibeer@gmail.com>
2019-03-22 12:04:27 +0100

    fix php cs

--- tests/EmojiTest.php
+++ tests/EmojiTest.php
@@ -95,7 +95,7 @@ class EmojiTest extends TestCase
 
     private function unicodeHexToEmoji(string $code)
     {
-        return mb_convert_encoding(hex2bin(implode('', array_map(function($hex) {
+        return mb_convert_encoding(hex2bin(implode('', array_map(function ($hex) {
             return str_pad($hex, 8, '0', STR_PAD_LEFT);
         }, explode(' ', trim(str_replace('}\u{', ' ', $code), '}\u{'))))), 'UTF-8', 'UTF-32');
     }
