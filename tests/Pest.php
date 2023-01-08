b49f063f30fa464843de54dce5aacf1e968720c8
Alex Manase <malexandru699@gmail.com>
2023-01-08 17:04:48 +0200

    refactor: EmojiTest

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,45 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Test Case
-|--------------------------------------------------------------------------
-|
-| The closure you provide to your test functions is always bound to a specific PHPUnit test
-| case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
-| need to change it using the "uses()" function to bind a different classes or traits.
-|
-*/
+uses(PHPUnit\Framework\TestCase::class)->in('.');
 
-// uses(Tests\TestCase::class)->in('Feature');
+// Functions
 
-/*
-|--------------------------------------------------------------------------
-| Expectations
-|--------------------------------------------------------------------------
-|
-| When you're writing tests, you often need to check that values meet certain conditions. The
-| "expect()" function gives you access to a set of "expectations" methods that you can use
-| to assert different things. Of course, you may extend the Expectation API at any time.
-|
-*/
-
-expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
-});
-
-/*
-|--------------------------------------------------------------------------
-| Functions
-|--------------------------------------------------------------------------
-|
-| While Pest is very powerful out-of-the-box, you may have some testing code specific to your
-| project that you don't want to repeat in every file. Here you can also expose helpers as
-| global functions to help you to reduce the number of lines of code in your test files.
-|
-*/
-
-function something()
+function codeToCallableProvider(): array
 {
-    // ..
+    return json_decode(file_get_contents(__DIR__ . '/emojis.json'), true);
+}
+
+function unicodeHexToEmoji(string $code)
+{
+    return mb_convert_encoding(hex2bin(implode('', array_map(function ($hex) {
+        return str_pad($hex, 8, '0', STR_PAD_LEFT);
+    }, explode(' ', trim(str_replace('}\u{', ' ', $code), '}\u{'))))), 'UTF-8', 'UTF-32');
 }
